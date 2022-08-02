3af666128c4951bb2d17b119e4344f31e1c52d9b
adaahmetyasin <adaahmetyasinedu@gmail.com>
2022-08-02 19:41:36 +0300

    search bar with react

---
+++ searchbar/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import SearchBar from './Components/SearchBar';
+import BookData from "./Data.json";
+
+function App() {
+  return  <div className="App">
+    <SearchBar placeholder="Enter a book name .." data={BookData} />
+  </div>
+}
+
+export default App;
